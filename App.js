beb1d4555171914a0a7ad0a8e98989fc1538a536
Drew <drc920630@gmail.com>
2023-07-07 00:38:09 +0900

    리액트네이티브 세팅완료

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>대답좀</Text>
+      <Text>zz</Text>
       <StatusBar style="auto" />
     </View>
   );

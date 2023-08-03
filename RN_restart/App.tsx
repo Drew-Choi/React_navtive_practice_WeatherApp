40f2382514048eede7c7a898aff1ceba16707ffb
Drew <drc920630@gmail.com>
2023-08-04 03:25:10 +0900

    서드파트트API로 위치정보 얻기 완료

--- RN_restart/App.tsx
+++ RN_restart/App.tsx
@@ -1,17 +1,60 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+// tyep ----
+interface LocationData {
+  country: string | null;
+  city: string | null;
+  district: string | null;
+}
+// ---------
+
 const App = () => {
-  const [location, setLocation] = useState(null)
+  const [location, setLocation] = useState<LocationData | null>(null);
+
+  const [ok, setOk] = useState<boolean>(true);
+
+  useEffect(() => {
+    const ask = async () => {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        setOk(false);
+      }
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+      const locationData = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+
+      if (locationData) {
+        let locationDataMake = {
+          country: locationData[0].country,
+          city: locationData[0].city,
+          district: locationData[0].district,
+        };
+
+        setLocation(locationDataMake);
+      }
+    };
+    ask();
+  }, []);
 
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>
+          {location === null
+            ? "Loading..."
+            : `${location.country} ${location.city} ${location.district}`}
+        </Text>
       </View>
       <ScrollView
         pagingEnabled
@@ -55,7 +98,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cityName: {
-    fontSize: 68,
+    fontSize: 25,
     fontWeight: "500",
   },
   weather: {},

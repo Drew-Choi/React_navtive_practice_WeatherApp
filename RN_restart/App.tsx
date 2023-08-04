a009873727aa024503035671c108a9b0b4977c20
Drew <drc920630@gmail.com>
2023-08-04 20:18:31 +0900

    날씨 정보 API구성 완료

--- RN_restart/App.tsx
+++ RN_restart/App.tsx
@@ -1,23 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import axios from "axios";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 // tyep ----
+// 로케이션 데이터
 interface LocationData {
   country: string | null;
   city: string | null;
   district: string | null;
 }
+
+// 날씨 데이터
+interface Data {
+  weather: { description: string }[];
+}
 // ---------
 
 const App = () => {
   const [location, setLocation] = useState<LocationData | null>(null);
-
   const [ok, setOk] = useState<boolean>(true);
+  const [days, setDays] = useState<Array<string> | null>(null);
 
+  const { EXPO_PUBLIC_WEATHER_API_KEY } = process.env;
+
+  // 위치 API요청 // useEffect Hook
   useEffect(() => {
     const ask = async () => {
       const { granted } = await Location.requestForegroundPermissionsAsync();
@@ -40,8 +57,32 @@ const App = () => {
           district: locationData[0].district,
         };
 
-        setLocation(locationDataMake);
+        // setLocation(locationDataMake);
       }
+
+      // 5일치, 3시간 간격의 날씨정보 API 요청
+      const response = await axios.get(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${EXPO_PUBLIC_WEATHER_API_KEY}`
+      );
+
+      // 필요한건 3시간 간격의 날씨가 아니라 데일리 날씨라 00시 기준으로
+      // 데이터 필터링
+      let pre = 0;
+      const filterData = await response.data.list
+        .filter((_: Data, index: number) => {
+          if (index === 0) {
+            return true;
+          } else if (index === 4) {
+            pre = index;
+            return true;
+          } else if (index === pre + 8) {
+            pre = index;
+            return true;
+          }
+        })
+        .map((el: Data) => el.weather[0].description);
+
+      setDays(filterData);
     };
     ask();
   }, []);
@@ -51,9 +92,11 @@ const App = () => {
       <StatusBar style="dark" />
       <View style={styles.city}>
         <Text style={styles.cityName}>
-          {location === null
-            ? "Loading..."
-            : `${location.country} ${location.city} ${location.district}`}
+          {location === null ? (
+            <ActivityIndicator />
+          ) : (
+            `${location.country} ${location.city} ${location.district}`
+          )}
         </Text>
       </View>
       <ScrollView
@@ -62,22 +105,6 @@ const App = () => {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>sunny</Text>
-        </View>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.desc}>sunny</Text>

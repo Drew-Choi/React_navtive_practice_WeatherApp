c9b03d3d4c60d19d78ab99a2afd2af3d253d6613
Drew <drc920630@gmail.com>
2023-08-06 18:57:22 +0900

    날씨 정보 정리 완료

--- RN_restart/App.tsx
+++ RN_restart/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -17,6 +17,7 @@ const App = () => {
   const [location, setLocation] = useState<LocationData | null>(null);
   const [ok, setOk] = useState<boolean>(true);
   const [days, setDays] = useState<Array<DatesObject>>([]);
+  const [currentTime, setCurrentTime] = useState<any | null>(null);
 
   const { EXPO_PUBLIC_WEATHER_API_KEY } = process.env;
 
@@ -31,19 +32,33 @@ const App = () => {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync({ accuracy: 5 });
 
-      const locationData = await Location.reverseGeocodeAsync(
-        { latitude, longitude },
-        { useGoogleMaps: false }
-      );
+      const [{ country, city, district, timezone }] =
+        await Location.reverseGeocodeAsync(
+          { latitude, longitude },
+          { useGoogleMaps: false }
+        );
 
-      if (locationData) {
+      if (country && timezone) {
+        // 사용자 국가 & 도시 & 동네 등
         let locationDataMake = {
-          country: locationData[0].country,
-          city: locationData[0].city,
-          district: locationData[0].district,
+          country,
+          city,
+          district,
         };
+        // 시간대별 날씨를 보여주기 위한 사용자 현재 시각 구하기
+        const nowUTC = new Date();
+        // UTC형식을 timezone기준으로 바꾸기, timezone은 reverseGeocodeAsync API에서 얻을 수 있음
+        const formatting = new Intl.DateTimeFormat("default", {
+          timeZone: timezone,
+          hour: "numeric",
+          hourCycle: "h23",
+        });
+        // UTC형식을 사용자 locale에 맞춰 바꾸기
+        const currentTime = formatting.format(nowUTC);
 
+        // 모든 데이터를 state로 보관
         setLocation(locationDataMake);
+        setCurrentTime(currentTime);
       }
 
       // 5일치, 3시간 간격의 날씨정보 API 요청
@@ -76,20 +91,15 @@ const App = () => {
           return obj;
         }
       );
-
-      console.log(datesArr);
-
       setDays(datesArr);
     };
     ask();
   }, []);
 
-  console.log(new Date());
-
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
-      <View style={styles.city}>
+      <View style={styles.cityContainer}>
         {location === null ? (
           <ActivityIndicator size="large" color="red" />
         ) : (
@@ -105,7 +115,6 @@ const App = () => {
         pagingEnabled
         horizontal
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
       >
         {days.length === 0 ? (
           <View style={styles.day}>
@@ -119,9 +128,31 @@ const App = () => {
           days.map((el, index) => (
             <View style={styles.day} key={index}>
               <Text style={styles.date}>{el.date}</Text>
-              {el.elements.map((time) => (
-                <Text style={styles.desc}>{time.time}</Text>
-              ))}
+              {el.elements.map((item, idx) =>
+                index === 0 ? (
+                  <View key={idx}>
+                    <Text style={styles.temp}>
+                      {currentTime >= item.time && item.time + 3 > currentTime
+                        ? item.temp.toFixed(1)
+                        : null}
+                    </Text>
+                    <Text style={styles.desc}>
+                      {currentTime >= item.time && item.time + 3 > currentTime
+                        ? item.weather
+                        : null}
+                    </Text>
+                  </View>
+                ) : (
+                  <View key={idx}>
+                    <Text style={styles.temp}>
+                      {item.time === 9 ? item.temp.toFixed(1) : null}
+                    </Text>
+                    <Text style={styles.desc}>
+                      {item.time === 9 ? item.weather : null}
+                    </Text>
+                  </View>
+                )
+              )}
             </View>
           ))
         )}
@@ -135,7 +166,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fce7dc",
   },
-  city: {
+  cityContainer: {
     flex: 1.2,
     justifyContent: "center",
     alignItems: "center",
@@ -149,14 +180,18 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "500",
   },
-  weather: {},
   day: {
     width: SCREEN_WIDTH,
     // backgroundColor: "red",
   },
+  tempContainer: {
+    backgroundColor: "red",
+    flexDirection: "row",
+    alignItems: "center",
+  },
   date: { fontSize: 20, paddingLeft: 20 },
   temp: { fontSize: 140, paddingLeft: 20 },
-  desc: { marginTop: -20, fontSize: 25, paddingLeft: 40 },
+  desc: { marginTop: -20, fontSize: 25, paddingLeft: 30, color: "gray" },
 });
 
 export default App;

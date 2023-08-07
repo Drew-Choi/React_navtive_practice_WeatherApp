beb88c2916ffb4aaabf6009874084952aa08df2d
Drew <drc920630@gmail.com>
2023-08-07 18:38:31 +0900

    reactnative CLI로 안드로이드 IOS 빌드 완료

--- RN_CLI/App.tsx
+++ RN_CLI/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
 
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeMessage}>higggg</Text>
+      <StatusBar />
+      <Text style={styles.welcomeMessage}>둘다 연동 된다이야 신기</Text>
     </View>
   );
 };
